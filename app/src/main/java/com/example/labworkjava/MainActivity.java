460b51d9612de4457b029526926925ec2023ac96
Yehor Poliarskyi <egoraua@gmail.com>
2023-10-02 23:14:54 +0300

    Revert "error"
    This reverts commit 529b042df6ad705222b45715192fe1f5e33bb4da.

--- app/src/main/java/com/example/labworkjava/MainActivity.java
+++ app/src/main/java/com/example/labworkjava/MainActivity.java
@@ -6,22 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    Button showBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView = (TextView) findViewById(R.id.HelloWorld);
-        showBtn = (Button) findViewById(R.id.button);
-        showBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) { textView.setText("my name is Alex");
-            }
-        });
-
-
     }
 }
