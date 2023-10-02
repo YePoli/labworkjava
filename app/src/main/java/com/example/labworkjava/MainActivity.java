529b042df6ad705222b45715192fe1f5e33bb4da
Yehor Poliarskyi <egoraua@gmail.com>
2023-10-02 23:14:27 +0300

    error

--- app/src/main/java/com/example/labworkjava/MainActivity.java
+++ app/src/main/java/com/example/labworkjava/MainActivity.java
@@ -6,9 +6,22 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    Button showBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.HelloWorld);
+        showBtn = (Button) findViewById(R.id.button);
+        showBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { textView.setText("my name is Alex");
+            }
+        });
+
+
     }
 }
