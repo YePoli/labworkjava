3c6251ee32a3a9e201138205715f4dcf7a2cbdb6
Yehor Poliarskyi <egoraua@gmail.com>
2023-10-02 23:32:19 +0300

    3 commit
    3 commit

--- app/src/main/java/com/example/labworkjava/MainActivity.java
+++ app/src/main/java/com/example/labworkjava/MainActivity.java
@@ -7,16 +7,24 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 TextView textview;
 Button button2;
+Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.Hell);
         button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 textView.setText("Yehor");
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setTextColor(1);
             }
         });
     }
