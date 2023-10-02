67462b8082c7b5b7675d2696b4f22982701c9c3f
Yehor Poliarskyi <egoraua@gmail.com>
2023-10-02 23:29:08 +0300

    error2
    second commit

--- app/src/main/java/com/example/labworkjava/MainActivity.java
+++ app/src/main/java/com/example/labworkjava/MainActivity.java
@@ -5,10 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView textview;
+Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.Hell);
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Yehor");
+            }
+        });
     }
 }
